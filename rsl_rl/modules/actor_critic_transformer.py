8745aca74bc0164111037a4e61559fdd0d60c514
kaixi287 <bao.kaixi@gmail.com>
2024-05-05 15:10:28 +0200

    Add customized implementation of transformer

--- rsl_rl/modules/actor_critic_transformer.py
+++ rsl_rl/modules/actor_critic_transformer.py
@@ -65,6 +65,97 @@ class ActorCriticTransformer(ActorCritic):
         input_c = self.memory_c(critic_observations, masks)
         return super().evaluate(input_c.squeeze(0))
 
+class LayerNormalization(nn.Module):
+
+    def __init__(self, features: int, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # alpha is a learnable parameter
+        self.bias = nn.Parameter(torch.zeros(features)) # bias is a learnable parameter
+
+    def forward(self, x):
+        # x: (batch, seq_len, hidden_size)
+         # Keep the dimension for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # Keep the dimension for broadcasting
+        std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+class ResidualConnection(nn.Module):
+    
+        def __init__(self, features: int, dropout: float) -> None:
+            super().__init__()
+            self.dropout = nn.Dropout(dropout)
+            self.norm = LayerNormalization(features)
+    
+        def forward(self, x, sublayer):
+            return x + self.dropout(sublayer(self.norm(x)))
+        
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+
+    def forward(self, x):
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, num_heads: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model # Embedding vector size
+        self.num_heads = num_heads
+        # Make sure d_model is divisible by num_heads
+        assert d_model % num_heads == 0, "d_model is not divisible by num_heads"
+
+        self.d_k = d_model // num_heads # Dimension of vector seen by each head
+        self.w_q = nn.Linear(d_model, d_model, bias=False) # Wq
+        self.w_k = nn.Linear(d_model, d_model, bias=False) # Wk
+        self.w_v = nn.Linear(d_model, d_model, bias=False) # Wv
+        self.w_o = nn.Linear(d_model, d_model, bias=False) # Wo
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        # Just apply the formula from the paper
+        # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            # Write a very low value (indicating -inf) to the positions where mask == 0
+            attention_scores.masked_fill_(~mask, float('-inf'))
+        attention_scores = attention_scores.softmax(dim=-1) # (batch, h, seq_len, seq_len) # Apply softmax
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        # (batch, h, seq_len, seq_len) --> (batch, h, seq_len, d_k)
+        # return attention scores which can be used for visualization
+        return (attention_scores @ value), attention_scores
+    
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        key = self.w_k(k) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        value = self.w_v(v) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+
+        # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        # Calculate attention
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+        
+        # Combine all the heads together
+        # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.num_heads * self.d_k)
+
+        # Multiply by Wo
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
+        return self.w_o(x)
 
 class PositionalEncoding(nn.Module):
     
@@ -109,6 +200,31 @@ class ExtendedEmbedding(nn.Module):
         x = self.linear2(x)
         return x
 
+class EncoderBlock(nn.Module):
+
+    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+    
+class Encoder(nn.Module):
+
+    def __init__(self, features: int, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
 class TransformerMemory(nn.Module):
     def __init__(self, input_dim, num_heads, num_layers, d_model: int = 256, d_ff: int = 1024, dropout: float = 0.1):
         super().__init__()
@@ -116,12 +232,15 @@ class TransformerMemory(nn.Module):
         self.embedding = nn.Linear(input_dim, d_model)
         # self.embedding = ExtendedEmbedding(input_dim, d_model, activation, d_embed)
         self.pos_encoder = PositionalEncoding(d_model, dropout)
-        transformer_layer =nn.TransformerEncoderLayer(d_model=d_model,
-                                                      nhead=num_heads,
-                                                      dim_feedforward=d_ff,
-                                                      dropout=dropout
-                                                      )
-        self.transformer_encoder = nn.TransformerEncoder(transformer_layer, num_layers)
+        # Create the encoder blocks
+        encoder_blocks = []
+        for _ in range(num_layers):
+            encoder_self_attention_block = MultiHeadAttentionBlock(d_model, num_heads, dropout)
+            feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+            encoder_block = EncoderBlock(d_model, encoder_self_attention_block, feed_forward_block, dropout)
+            encoder_blocks.append(encoder_block)
+        
+        self.transformer_encoder = Encoder(d_model, nn.ModuleList(encoder_blocks))
         # self.initialize_transformer()
     
     def initialize_transformer(self):
@@ -129,29 +248,37 @@ class TransformerMemory(nn.Module):
             if p.dim() > 1:  # Applies to weights of linear layers and not biases
                 nn.init.xavier_uniform_(p)
     
+    def generate_causal_mask(self, size):
+        mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+        return mask == 0
+    
     def forward(self, x, masks=None):
 
         if masks is not None:
             # Training mode
-            # Padding mask should be (batch_size, seq_len), with True values for positions to ignore
-            padding_masks = torch.where(masks.t().float() == 0, torch.tensor(float('-inf'), device=x.device), torch.tensor(0.0, device=x.device))
+            x = x.transpose(0, 1) # (seq_len, batch_size, num_obs) --> (batch_size, seq_len, num_obs)
+            seq_len = x.size(1)
+            # Padding mask (seq_len, batch_size) --> (batch_size, seq_len), with False values for padded positions
+            padding_mask = masks.t()
+            # Unsqueeze and expand to match the dimensions of the causal mask
+            padding_mask = padding_mask.unsqueeze(1)  # Add a singleton dimension for head
+            padding_mask = padding_mask.expand(-1, seq_len, -1)  # Expand across the seq_len dimension
         else:
             # Inference mode
             if x.dim() < 3:
-                x = x.unsqueeze(0)  # Adjust for seq_len dimension in inference
-            padding_masks = None
+                x = x.unsqueeze(1)  # Adjust for seq_len dimension in inference
+            seq_len = x.size(1)
+            padding_mask = None
         
-        seq_len = x.size(0)
-
         # Generate a causal mask to limit attention to the preceding tokens
-        causal_mask = nn.Transformer.generate_square_subsequent_mask(seq_len, device=x.device)
+        causal_mask = self.generate_causal_mask(seq_len).to(x.device)   # (1, seq_len, seq_len)
 
         # Embed the input (seq_len, batch_size, num_obs) --> (seq_len, batch_size, d_model)
         x = self.embedding(x)
         x = self.pos_encoder(x) # (seq_len, batch_size, d_model)
 
         # Pass through the transformer.
-        x = self.transformer_encoder(x, mask=causal_mask, src_key_padding_mask=padding_masks)   # (seq_len, batch_size, d_model)
+        x = self.transformer_encoder(x, mask=(padding_mask & causal_mask))   # (seq_len, batch_size, d_model)
         
         if masks is not None:
             x = unpad_trajectories(x, masks)
